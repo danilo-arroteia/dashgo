22d11f7ee6527e4e3430ea343c97756e8358f072
Shyumi123 <sikor12@gmail.com>
2022-08-24 09:37:27 -0300

    feat: finish Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
-import { Flex, Icon, Input, Text } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Icon, Input, Text, HStack, Box, Avatar } from "@chakra-ui/react";
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine,
+} from "react-icons/ri";
 
 export function Header() {
   return (
-    <Flex 
-      as="header" 
+    <Flex
+      as="header"
       w="100%"
       maxWidth={1480}
       h="20"
@@ -12,14 +16,12 @@ export function Header() {
       mt="4"
       px="6"
       align="center"
-     >
-      <Text
-        fontSize="3xl"
-        fontWeight="bold"
-        letterSpacing="tight"
-        w="64"
-      >
-        dashgo<Text as="span" ml="1" color="pink.500">.</Text>
+    >
+      <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+        dashgo
+        <Text as="span" ml="1" color="pink.500">
+          .
+        </Text>
       </Text>
 
       <Flex
@@ -35,17 +37,43 @@ export function Header() {
         bg="gray.800"
         borderRadius="full"
       >
-        <Input 
+        <Input
           color="gray.50"
           variant="unstyled"
           px="4"
           mr="4"
-          placeholder='Search the platform'
-          _placeholder={{ color: 'gray.400' }}
+          placeholder="Search the platform"
+          _placeholder={{ color: "gray.400" }}
         />
 
-        <Icon as={RiSearchLine} fontSize="20"/>
+        <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Danilo Arrotéia</Text>
+            <Text color="gray.300" fontSize="small">
+              danilo.arroteia@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar size="md" name="Danilo Arrotéia" src="https://www.github.com/danilo-arroteia.png"/>
+        </Flex>
       </Flex>
     </Flex>
-  )
+  );
 }
